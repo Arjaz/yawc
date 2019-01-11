1eb06ca4fa31ea26fd3f0522cb4c338e8844611a
Arjaz <arjaz@protonmail.com>
2019-01-11 02:37:03 +0200

    Counter for individual words added

--- yawc/views.py
+++ yawc/views.py
@@ -7,5 +7,16 @@ def index(request):
 
 def count(request):
     fulltext = request.GET['fulltext']
-    w_number = fulltext.count(' ') + 1
-    return render(request, 'count.html', {'fulltext': fulltext, 'w_number': w_number})
+    words = fulltext.split()
+    w_number = len(words)
+
+    word_usage = dict()
+    
+    for word in words:
+        if word in word_usage:
+            word_usage[word] += 1
+        else:
+            word_usage[word] = 1
+    
+    sorted_words = sorted(word_usage.items(), key=lambda x: x[1], reverse=True)
+    return render(request, 'count.html', {'fulltext': fulltext, 'w_number': w_number, 'word_usage': sorted_words})
